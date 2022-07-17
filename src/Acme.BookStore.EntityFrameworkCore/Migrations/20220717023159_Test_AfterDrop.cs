d6ff508ff2e942a1401476f64b5dc47322ad1faf
USsefRashed <USsefRashed@gmail.com>
2022-07-16 19:40:25 -0700

    Add project files.

---
+++ src/Acme.BookStore.EntityFrameworkCore/Migrations/20220717023159_Test_AfterDrop.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Acme.BookStore.Migrations
+{
+    public partial class Test_AfterDrop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
